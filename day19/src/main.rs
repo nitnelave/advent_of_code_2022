eb299bf543255b67f9b789fdd5ec02244541acfd
Valentin Tolmer <valentin@tolmer.fr>
2022-12-19 10:27:37 +0100

    day19, fast

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -32,79 +32,91 @@ fn parse_blueprint(line: String) -> Blueprint {
 struct State {
     ores: [OreAmount; 4],
     robots: [u16; 4],
-    time: usize,
+    time: u16,
 }
 
 fn recurse_simulation(
     blueprint: &Blueprint,
     state: State,
-    max_time: usize,
+    max_time: u16,
     max_robots: &[u16; 4],
-) -> OreAmount {
-    let mut max_geodes = 0;
-    for i in 0..blueprint.recipes.len() {
+    max_geodes: &mut OreAmount,
+) {
+    let mut has_recursed = false;
+    for i in 0..4 {
         if state.robots[i] == max_robots[i] {
             continue;
         }
-        if i < 2 && state.robots[i + 2] > 2 {
-            continue;
-        }
         let recipe = &blueprint.recipes[i];
-        if let Some(wait_time) = (0..recipe.len())
+        // Find the limiting ore for the recipe.
+        let wait_time = match (0..recipe.len())
             .filter_map(|ore_type| {
                 if recipe[ore_type] == 0 {
                     None
+                } else if recipe[ore_type] <= state.ores[ore_type] {
+                    Some(0)
+                } else if state.robots[ore_type] == 0 {
+                    // No robot yet, we can't build it (it takes more than max_time to build it).
+                    Some(max_time as u16 + 1)
                 } else {
-                    if recipe[ore_type] <= state.ores[ore_type] {
-                        Some(0)
-                    } else if state.robots[ore_type] == 0 {
-                        Some(max_time as u16 + 1)
-                    } else {
-                        Some(
-                            (recipe[ore_type] - state.ores[ore_type] + state.robots[ore_type] - 1)
-                                / state.robots[ore_type],
-                        )
-                    }
+                    Some(
+                        (recipe[ore_type] - state.ores[ore_type] + state.robots[ore_type] - 1)
+                            / state.robots[ore_type],
+                    )
                 }
             })
             .max()
         {
-            let time_finished = state.time + wait_time as usize + 1;
-            if time_finished < max_time {
-                let mut new_ores = [0; 4];
-                let mut new_robots = [0; 4];
-                for o in 0..4 {
-                    new_ores[o] = state.ores[o] + state.robots[o] * (wait_time + 1) - recipe[o];
-                    new_robots[o] = state.robots[o] + if o == i { 1 } else { 0 };
-                }
-                max_geodes = std::cmp::max(
-                    max_geodes,
-                    recurse_simulation(
-                        blueprint,
-                        State {
-                            ores: new_ores,
-                            robots: new_robots,
-                            time: time_finished,
-                        },
-                        max_time,
-                        max_robots,
-                    ),
-                );
-            }
+            None => continue,
+            Some(wait_time) => wait_time,
+        };
+        let time_finished = state.time + wait_time + 1;
+        if time_finished >= max_time {
+            continue;
         }
+        let mut new_ores = [0; 4];
+        let mut new_robots = [0; 4];
+        for o in 0..4 {
+            new_ores[o] = state.ores[o] + state.robots[o] * (wait_time + 1) - recipe[o];
+            new_robots[o] = state.robots[o] + u16::from(o == i);
+        }
+        let remaining_time = max_time - time_finished;
+        // If we were to build only geode robots every turn, could we beat the current max?
+        if ((remaining_time - 1) * remaining_time) / 2
+            + new_ores[3]
+            + remaining_time * new_robots[3]
+            < *max_geodes
+        {
+            continue;
+        }
+        has_recursed = true;
+        recurse_simulation(
+            blueprint,
+            State {
+                ores: new_ores,
+                robots: new_robots,
+                time: time_finished,
+            },
+            max_time,
+            max_robots,
+            max_geodes,
+        );
     }
-    if max_geodes == 0 {
-        state.ores[3] + state.robots[3] * (max_time - state.time) as u16
-    } else {
-        max_geodes
+    if !has_recursed {
+        // We couldn't make new robots, so this is the best this branch can do.
+        *max_geodes = std::cmp::max(
+            *max_geodes,
+            state.ores[3] + state.robots[3] * (max_time - state.time) as u16,
+        );
     }
 }
 
-fn simulate_blueprint(blueprint: &Blueprint, max_time: usize) -> OreAmount {
+fn simulate_blueprint(blueprint: &Blueprint, max_time: u16) -> OreAmount {
     let mut max_robots = [u16::max_value(); 4];
     for i in 0..3 {
         max_robots[i] = blueprint.recipes.iter().map(|r| r[i]).max().unwrap();
     }
+    let mut max_geodes = 0;
     recurse_simulation(
         blueprint,
         State {
@@ -114,7 +126,9 @@ fn simulate_blueprint(blueprint: &Blueprint, max_time: usize) -> OreAmount {
         },
         max_time,
         &max_robots,
-    )
+        &mut max_geodes,
+    );
+    max_geodes
 }
 
 fn main() {
